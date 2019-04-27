1b88731d7b39624aa5b12d0b027c40c0ca2ed721
jasonliao001 <jasonliao1992@163.com>
2019-04-28 00:59:43 +0800

    update

--- examples/router.js
+++ examples/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 import message from './docs/message.md';
 
 Vue.use(Router);
-
+// 上传到github不能使用history模式
 export default new Router({
   // mode: 'history',
   routes: [
